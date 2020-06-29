1e5bd24ed5ba9a14c306fd02c9555dce2b3f7b2c
A D <amitdesh2014@gmail.com>
2020-06-29 19:28:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_key = ""
   x = 100
   name_hash.each do |key, value|
-    if name_hash[0] < x
+    if name_hash[key] < x
       min_key = name_hash[key]
     end
   end
