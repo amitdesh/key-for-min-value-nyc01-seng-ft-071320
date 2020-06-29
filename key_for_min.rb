b606093859ab979489a86ef260804b3bd8c375f3
A D <amitdesh2014@gmail.com>
2020-06-29 19:27:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,10 @@ def key_for_min_value(name_hash)
   min_key = ""
   x = 100
   name_hash.each do |key, value|
-    if name_hash[key] < x
+    if name_hash[0] < x
       min_key = name_hash[key]
     end
   end
-  name_hash.select {|key| name_hash[key] == min_key}
+  ans = name_hash.select {|key| name_hash[key] == min_key}
+  return ans[0]
 end
